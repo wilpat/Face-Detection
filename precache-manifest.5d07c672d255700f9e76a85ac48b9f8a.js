929e3735452a9c3d3e669316572bd670b6d59a7a
William Okafor <wilpat456@gmail.com>
2019-02-13 00:27:11 +0100

    Updates

---
+++ precache-manifest.5d07c672d255700f9e76a85ac48b9f8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90453402bce0f11af1d1",
+    "url": "/Face-Detection/static/js/runtime~main.90453402.js"
+  },
+  {
+    "revision": "fd48fcd984b8960f2035",
+    "url": "/Face-Detection/static/js/main.fd48fcd9.chunk.js"
+  },
+  {
+    "revision": "c7ccc9577b82f881b8f8",
+    "url": "/Face-Detection/static/js/1.c7ccc957.chunk.js"
+  },
+  {
+    "revision": "fd48fcd984b8960f2035",
+    "url": "/Face-Detection/static/css/main.592f1b2f.chunk.css"
+  },
+  {
+    "revision": "c7ccc9577b82f881b8f8",
+    "url": "/Face-Detection/static/css/1.b8cb2a35.chunk.css"
+  },
+  {
+    "revision": "5919ce58172a960a51f8a0e42e31ea66",
+    "url": "/Face-Detection/index.html"
+  }
+];
